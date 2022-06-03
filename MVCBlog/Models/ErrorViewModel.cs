af3453cd91327e9b50d9008f631e416dbfa0d877
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-03 11:19:14 +0300

    Add project files.

---
+++ MVCBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
