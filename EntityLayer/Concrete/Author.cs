48670fbd7bb9c737b259afd0a4d851f47b117fa8
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-03 11:46:43 +0300

    Category and Author Class added

---
+++ EntityLayer/Concrete/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string About { get; set; }
+    }
+}
